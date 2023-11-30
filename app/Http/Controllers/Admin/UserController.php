746a2fca5d04897a9dbfe38f8e3e6405bb497fc5
Aadil Kansakar <aadilkansakar@gmail.com>
2023-11-30 16:52:09 +0545

    users create from to db

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -13,5 +14,14 @@ class UserController extends Controller
         $users = User::latest()->get();
 
         return $users;
+    }
+
+    public function store(Request $request)
+    {
+        return User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
     }
 }
--- resources/js/pages/users/ListUsers.vue
+++ resources/js/pages/users/ListUsers.vue
@@ -1,13 +1,30 @@
 <script setup>
     import axios from "axios";
-    import { ref, onMounted } from "vue";
+    import { ref, onMounted, reactive } from "vue";
 
     const users = ref([]);
+
+    const form = reactive({
+        name: '',
+        email: '',
+        password: ''
+    });
 
     const getUsers = () => {
         axios.get('/api/users')
         .then((response) => {
             users.value = response.data;
+        });
+    };
+
+    const createUser = () => {
+        axios.post('/api/users', form)
+        .then((response) => {
+            users.value.unshift(response.data);
+            form.name = '';
+            form.email = '';
+            form.password = '';
+            $('#createUserModal').modal('hide');
         });
     };
 
@@ -36,6 +53,10 @@
 
     <div class="content">
         <div class="container-fluid">
+            <!-- Button trigger modal -->
+            <button type="button" class="mb-2 btn btn-primary" data-toggle="modal" data-target="#createUserModal">
+                Add New User
+            </button>
             <div class="card">
                 <div class="card-body">
                     <table class="table table-bordered">
@@ -60,6 +81,46 @@
                             </tr>
                         </tbody>
                     </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="createUserModal" data-backdrop="static" tabindex="-1" role="dialog"
+        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">Add New User</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form autocomplete="off">
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input v-model="form.name" type="text" class="form-control " id="name"
+                                aria-describedby="nameHelp" placeholder="Enter full name">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input v-model="form.email" type="email" class="form-control " id="email"
+                                aria-describedby="nameHelp" placeholder="Enter full name">
+                        </div>
+                    </form>
+
+                    <div class="form-group">
+                        <label for="email">Password</label>
+                        <input v-modal="form.password" type="password" class="form-control " id="password"
+                            aria-describedby="nameHelp" placeholder="Enter password">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                    <button @click="createUser()" type="button" class="btn btn-primary">Save</button>
                 </div>
             </div>
         </div>
