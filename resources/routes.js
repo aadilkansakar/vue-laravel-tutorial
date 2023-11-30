53d24b7682a284cbbc12f3ce8f6b527f25ddaf4b
Aadil Kansakar <aadilkansakar@gmail.com>
2023-11-30 14:31:00 +0545

    multiple pages created for routers

--- resources/routes.js
+++ resources/routes.js
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Dashboard from "./js/components/Dashboard.vue";
+import ListAppointments from "./js/pages/appointments/ListAppointments.vue";
+import ListUsers from "./js/pages/users/ListUsers.vue";
+import UpdateSetting from "./js/pages/settings/UpdateSetting.vue";
+import UpdateProfile from "./js/pages/profile/UpdateProfile.vue";
 
 const router = createRouter({
     history: createWebHistory(),
@@ -8,6 +12,26 @@ const router = createRouter({
             path: '/admin/dashboard',
             name: 'admin.dashboard',
             component: Dashboard
+        },
+        {
+            path: '/admin/appointments',
+            name: 'admin.appointments',
+            component: ListAppointments
+        }
+        {
+            path: '/admin/users',
+            name: 'admin.users',
+            component: ListUsers
+        }
+        {
+            path: '/admin/settings',
+            name: 'admin.settings',
+            component: UpdateSetting
+        }
+        {
+            path: '/admin/profile',
+            name: 'admin.profile',
+            component: UpdateProfile
         }
     ]
 });
